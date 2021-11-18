33c71bd27525d2967fabcdf5cf570beec8ac78a2
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2021-11-18 18:43:47 +0000

    coisas

--- aluno.c
+++ aluno.c
@@ -4,10 +4,10 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#include "util.h"
+//#include "util.h"
 
 int main(int argc, char **argv, char **envp) {
-    // configurar e lançar programa classificador
+    // configurar e lan�ar programa classificador
     int fd_in[2], fd_out[2];
     pipe(fd_in);
     pipe(fd_out);
@@ -33,12 +33,13 @@ int main(int argc, char **argv, char **envp) {
         close(fd_in[0]);
         close(fd_out[1]);
     }
-    // definição de variáveis
+    // defini��o de vari�veis
     int maxClientes, maxMedicos;
-    Utente utentes[5][5];
-    Utente utenteNovo;
+  //  Utente utentes[5][5];
+   // Utente utenteNovo;
     int filas[5] = {0, 0, 0, 0, 0};
-    char sintomas[512];
+    //alterei char  sintomas[]="doi tudo\n\0";
+    char sintomas[1000];
     int i, j;
 
     //parte das variaveis de ambiente
@@ -53,12 +54,13 @@ int main(int argc, char **argv, char **envp) {
     maxClientes = atoi(getenv("MAXCLIENTES"));
     maxMedicos = atoi(getenv("MAXMEDICOS"));
 
-    //classificação de especialidade e respetiva prioridade
+    //classifica��o de especialidade e respetiva prioridade
     int sair_while=0;
     printf("estpou aqui\n");
     while(sair_while < 3) {
         printf("Indique os sintomas: ");
-        fgets(sintomas, sizeof(sintomas), stdin);
+        fgets(sintomas, sizeof(sintomas)-1, stdin);
+       //alterei fgets(sintomas, sizeof(sintomas), stdin);
         char especialidade[256];
         int prioridade=0;
         char temp[256];
@@ -69,16 +71,22 @@ int main(int argc, char **argv, char **envp) {
         }
         
         // enviar sintomas ao classificador
-        write(to_class, sintomas, sizeof(sintomas));
+        //alterei write(to_class, sintomas, sizeof(sintomas));
+        write(to_class, sintomas, strlen(sintomas));
         // receber resposta do classificador
         printf("aqui\n");
-        debug_read = read(from_class, temp, sizeof(char)*512-1);
+        debug_read = read(from_class, temp, 255);
+        // alterei debug_read = read(from_class, temp, sizeof(temp)-1);
         printf("ali %d\n", debug_read);
+        if (debug_read == 0) {
+            printf("\na resposta est� vazia");
+            continue;
+        }
         temp[debug_read] = '\0';
         // separar resposta
-        sscanf(temp, "%s %d", especialidade, &prioridade);
-
-        printf("(%s || %s || %d)\n", temp, especialidade, prioridade);
+        //sscanf(temp, "%s %d", especialidade, &prioridade);
+        printf("Resposta: (%s)\n", temp);
+        //alterei printf("(%s || %s || %d)\n", temp, especialidade, prioridade);
         /*
         if(strcmp(especialidade, "geral") == 0 && filas[0] < 5) {
             strcpy(utenteNovo.especialidade, especialidade);
