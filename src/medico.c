1390b5c1fd258aec00f33d99dfe653be6e282673
ZMiguel Valdiviesso <zmiguel.alves@gmail.com>
2022-01-11 10:55:58 +0000

    basic pipe config

--- src/medico.c
+++ src/medico.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+
+#include "util.h"
 
 int main(int argc, char **argv){
+    // Check if server is running
+    if (open("np_balcao", O_RDWR) == -1 ){
+        printf("Servidor não encontrado.\n");
+        return 0;
+    }
+    // Config pipes
+    int pid = getpid();
+    char *fifo = malloc(sizeof(char)*20);
+    sprintf(fifo, "./medico-%d", pid);
+    printf("%s\n", fifo);
+    char *server_fifo = "./np_balcao";
+    printf("PID: %d\n", pid);
+    mkfifo(fifo, 0666);
+
     return 0;
 }
