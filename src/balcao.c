1d3c82ac4e40cd479038c642cdeb28d26a85873e
Diogo Semião <diogo@diogo>
2022-01-11 21:41:06 +0000

    implementacao do select para o balcao (incompleto)

--- src/balcao.c
+++ src/balcao.c
@@ -3,8 +3,20 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 #include "util.h"
+
+#define SERVER_FIFO "np_balcao"
+#define CLIENTE_FIFO "np_cliente_%d"
+
+int max(int a, int b) {
+    return (a>b) ? a : b;
+}
 
 int main(int argc, char **argv, char **envp) {
     // configurar e lançar programa classificador
@@ -39,7 +51,11 @@ int main(int argc, char **argv, char **envp) {
     Utente utenteNovo;
     int filas[5] = {0, 0, 0, 0, 0};
     char sintomas[1000];
-    int i, j;
+    int i, j, res;
+    int s_fifo_fd;
+    fd_set read_fds;
+    int nfd;
+    struct timeval tv;
 
     // iniciar utentes a zero
     for(i = 0; i < 5; i++) {
@@ -61,90 +77,126 @@ int main(int argc, char **argv, char **envp) {
     maxClientes = atoi(getenv("MAXCLIENTES"));
     maxMedicos = atoi(getenv("MAXMEDICOS"));
 
+    U_B clientes[maxClientes];
+    M_B medicos[maxMedicos];
+
+    res = mkfifo(SERVER_FIFO, 0777);
+    if (res == -1) {
+        perror("\nmkfifo do FIFO do servidor deu erro");
+        exit(EXIT_FAILURE);
+    }
+    s_fifo_fd = open(SERVER_FIFO, O_RDWR | O_NONBLOCK);
+    if (s_fifo_fd == -1) {
+        perror("\nErro ao abrir o FIFO do servidor (RDWR/non blocking)");
+        exit(EXIT_FAILURE);
+    }
+
     //classificação de especialidade e respetiva prioridade
     while(1) {
-        printf("Indique os sintomas: ");
-        fgets(sintomas, sizeof(sintomas)-1, stdin);
-        char especialidade[256];
-        int prioridade=0;
-        char temp[256];
-        int debug_read = 0;
+        tv.tv_sec = 10;
+        tv.tv_usec = 0;
+        FD_ZERO(& read_fds);
+        FD_SET(0, & read_fds);
+        FD_SET(s_fifo_fd, & read_fds);
+        nfd = select(s_fifo_fd + 1, & read_fds, NULL, NULL, & tv);
+        if (nfd == 0) {
+            fflush(stdout);
+            continue;
+        }
+        if (nfd == -1) {
+            perror("\nerro no select");
+            close(s_fifo_fd);
+            unlink(SERVER_FIFO);
+            return EXIT_FAILURE;
+        }
 
-        if(strcmp(sintomas, "sair\n") == 0) {
-            char sair[] = "#fim\n";
-            write(to_class, sair, strlen(sair));
-            return 0;
-        }
-        
-        // enviar sintomas ao classificador
-        write(to_class, sintomas, strlen(sintomas));
-        // receber resposta do classificador
-        debug_read = read(from_class, temp, sizeof(temp)-1);
-        if(debug_read == -1) {
-            printf("erro ao ler do classificador\n");
-            return 0;
-        }
-        temp[debug_read] = '\0';
-        // separar resposta
-        sscanf(temp, "%s %d", especialidade, &prioridade);
-        
-        if(strcmp(especialidade, "geral") == 0 && filas[0] < 5) {
-            strcpy(utenteNovo.especialidade, especialidade);
-            utenteNovo.prioridade = prioridade;
-            strcpy(utenteNovo.nome, "John Doe");
-            utentes[0][filas[0]] = utenteNovo;
-            filas[0]++;
-        }
-        else {
-            if(strcmp(especialidade, "ortopedia") == 0 && filas[1] < 5) {
+        if (FD_ISSET(0, & read_fds)) {
+            printf("Indique os sintomas: ");
+            fgets(sintomas, sizeof(sintomas)-1, stdin);
+            char especialidade[256];
+            int prioridade=0;
+            char temp[256];
+            int debug_read = 0;
+
+            if(strcmp(sintomas, "sair\n") == 0) {
+                char sair[] = "#fim\n";
+                write(to_class, sair, strlen(sair));
+                return 0;
+            }
+            
+            // enviar sintomas ao classificador
+            write(to_class, sintomas, strlen(sintomas));
+            // receber resposta do classificador
+            debug_read = read(from_class, temp, sizeof(temp)-1);
+            if(debug_read == -1) {
+                printf("erro ao ler do classificador\n");
+                return 0;
+            }
+            temp[debug_read] = '\0';
+            // separar resposta
+            sscanf(temp, "%s %d", especialidade, &prioridade);
+            
+            if(strcmp(especialidade, "geral") == 0 && filas[0] < 5) {
                 strcpy(utenteNovo.especialidade, especialidade);
                 utenteNovo.prioridade = prioridade;
                 strcpy(utenteNovo.nome, "John Doe");
-                utentes[1][filas[1]] = utenteNovo;
-                filas[1]++;
+                utentes[0][filas[0]] = utenteNovo;
+                filas[0]++;
             }
             else {
-                if(strcmp(especialidade, "estomatologia") == 0 && filas[2] < 5) {
+                if(strcmp(especialidade, "ortopedia") == 0 && filas[1] < 5) {
                     strcpy(utenteNovo.especialidade, especialidade);
                     utenteNovo.prioridade = prioridade;
                     strcpy(utenteNovo.nome, "John Doe");
-                    utentes[2][filas[2]] = utenteNovo;
-                    filas[2]++;
+                    utentes[1][filas[1]] = utenteNovo;
+                    filas[1]++;
                 }
                 else {
-                    if(strcmp(especialidade, "neurologia") == 0 && filas[3] < 5) {
+                    if(strcmp(especialidade, "estomatologia") == 0 && filas[2] < 5) {
                         strcpy(utenteNovo.especialidade, especialidade);
                         utenteNovo.prioridade = prioridade;
                         strcpy(utenteNovo.nome, "John Doe");
-                        utentes[3][filas[3]] = utenteNovo;
-                        filas[3]++;
+                        utentes[2][filas[2]] = utenteNovo;
+                        filas[2]++;
                     }
                     else {
-                        if(strcmp(especialidade, "oftalmologia") == 0 && filas[4] < 5) {
+                        if(strcmp(especialidade, "neurologia") == 0 && filas[3] < 5) {
                             strcpy(utenteNovo.especialidade, especialidade);
                             utenteNovo.prioridade = prioridade;
                             strcpy(utenteNovo.nome, "John Doe");
-                            utentes[4][filas[4]] = utenteNovo;
-                            filas[4]++;
+                            utentes[3][filas[3]] = utenteNovo;
+                            filas[3]++;
                         }
                         else {
-                            printf("Especialidade nao existe ou filas cheias!\n");
-                            continue;
+                            if(strcmp(especialidade, "oftalmologia") == 0 && filas[4] < 5) {
+                                strcpy(utenteNovo.especialidade, especialidade);
+                                utenteNovo.prioridade = prioridade;
+                                strcpy(utenteNovo.nome, "John Doe");
+                                utentes[4][filas[4]] = utenteNovo;
+                                filas[4]++;
+                            }
+                            else {
+                                printf("Especialidade nao existe ou filas cheias!\n");
+                                continue;
+                            }
                         }
                     }
                 }
             }
-        }
-        printf("----------\nTodos os utentes:\n");
-        for(i=0;i<5;i++) {
-            for(j=0;j<5;j++) {
-                if(utentes[i][j].especialidade[0] != '\0') {
-                    printf("Especialidade: %s\tPrioridade: %d\n", utentes[i][j].especialidade, utentes[i][j].prioridade);
+            printf("----------\nTodos os utentes:\n");
+            for(i=0;i<5;i++) {
+                for(j=0;j<5;j++) {
+                    if(utentes[i][j].especialidade[0] != '\0') {
+                        printf("Especialidade: %s\tPrioridade: %d\n", utentes[i][j].especialidade, utentes[i][j].prioridade);
+                    }
                 }
             }
+            printf("----------\n");
         }
-        printf("----------\n");
     }
+
+    close(s_fifo_fd);
+    unlink(SERVER_FIFO);
 
     return 0;
 }
